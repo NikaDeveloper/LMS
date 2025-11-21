7b118aab77f243b140478ac6c75f8e3635cab513
Nikushka2024 <megrelynika@gmail.com>
2025-11-21 21:08:14 +0300

    в сериализатор добавлено поле вывода кол-ва уроков

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
+
 from .models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = SerializerMethodField()
+
     class Meta:
         model = Course
         fields = "__all__"
+
+    def get_lesson_count(self, obj):
+        return obj.lessons.count()
 
 
 class LessonSerializer(serializers.ModelSerializer):
