afd257f34177b42d992cfda62d8fec287825e6b9
Nikushka2024 <megrelynika@gmail.com>
2025-11-26 14:02:13 +0300

    views + urls

--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,21 @@
 from django.urls import path
-from .views import UserProfileRetrieveUpdateDestroyAPIView, PaymentListAPIView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import UserCreateAPIView, UserProfileRetrieveUpdateDestroyAPIView, PaymentListAPIView
 
 
 app_name = "users"
 
-
 urlpatterns = [
-    path(
-        "profile/<int:pk>/",
-        UserProfileRetrieveUpdateDestroyAPIView.as_view(),
-        name="user-profile-detail",
-    ),
+    # Регистрация
+    path('register/', UserCreateAPIView.as_view(), name='register'),
+
+    # Авторизация (получение токена)
+    path('token/', TokenObtainPairView.as_view(), name='token'),
+
+    # Обновление токена
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Профиль и платежи
+    path('profile/<int:pk>/', UserProfileRetrieveUpdateDestroyAPIView.as_view(), name='user-profile'),
     path('payments/', PaymentListAPIView.as_view(), name='payments-list'),
 ]
