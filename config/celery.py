522b25c2734445c6c9145adf2a91d6e23d7e12a8
Nikushka2024 <megrelynika@gmail.com>
2025-12-09 18:09:03 +0300

    realized block inactive users as a periodic task with celery-beat crontab

--- config/celery.py
+++ config/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
@@ -15,3 +16,10 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
+# Настройка периодических задач
+app.conf.beat_schedule = {
+    'block-inactive-users-every-day': {
+        'task': 'users.tasks.block_inactive_users', # Путь к задаче
+        'schedule': crontab(hour=0, minute=0), # Запуск каждый день в 00:00
+    },
+}
