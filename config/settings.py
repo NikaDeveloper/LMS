746bc4e503d3a9f83e2cf6131b466d26801948e8
Nikushka2024 <megrelynika@gmail.com>
2025-12-10 14:20:35 +0300

    Configuring SMTP for sending real emails

--- config/settings.py
+++ config/settings.py
@@ -103,7 +103,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT", 587)
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", True)
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 STATIC_URL = "static/"
 
